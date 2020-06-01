8b0b0dbd079d6554e987dbed2e07c4eac8cfb675
Social Codia <socialcodia@gmail.com>
2020-06-02 04:40:02 +0530

    Changes

--- index.php
+++ index.php
@@ -37,7 +37,6 @@ if(isset($_GET['q']))
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
-
     <body>
     <nav class="black">
     <a href="#" class="brand-logo">Github Explorer</a>
